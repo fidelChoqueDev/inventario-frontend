2efdc41e73193f32376f4e8e9ca64c3c5ff38fca
migueldecode <migueldecode@gmail.com>
2024-08-07 19:05:39 +0200

    feat: add back button to register page

--- src/pages/RegisterUserPage.tsx
+++ src/pages/RegisterUserPage.tsx
@@ -1,8 +1,10 @@
+import ButtonBackIcon from "../components/ButtonBackIcon/ButtonBackIcon.tsx"
 import RegisterForm from "../components/RegisterForm/RegisterForm.tsx"
 
 const CreateUserPage = () => {
   return (
     <main>
+      <ButtonBackIcon goTo="/"/>
       <img src="/clean-box.png" alt="Box" />
       <RegisterForm/>
     </main>
