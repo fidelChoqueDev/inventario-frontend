ca5ade111511a4227bd84884a45a8b79c5e47ec1
migueldecode <migueldecode@gmail.com>
2024-08-16 12:30:06 +0200

    feat: add logical of error message of set a new password

--- src/pages/NewPasswordPage.tsx
+++ src/pages/NewPasswordPage.tsx
@@ -1,8 +1,35 @@
+import { useRef, useState } from "react";
 import Button from "../components/Button/Button";
 import ButtonBackIcon from "../components/ButtonBackIcon/ButtonBackIcon";
 import InputPasswordForm from "../components/InputPasswordForm/InputPasswordForm";
+import ErrorMessage from "../components/ErrorMessage/ErrorMessage";
 
 const NewPasswordPage = () => {
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const passwordCheckRef = useRef<HTMLInputElement>(null);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const password = passwordRef.current?.value;
+    const passwordCheck = passwordCheckRef.current?.value;
+
+    if (!password || !passwordCheck) {
+      setErrorMessage("Ambos campos son obligatorios");
+      return;
+    }
+
+    if (password !== passwordCheck) {
+      setErrorMessage("Las contraseñas no son iguales");
+      return;
+    }
+
+    setErrorMessage(null);
+
+    // Send password to back
+  };
+
   return (
     <main>
       <ButtonBackIcon goTo="/" />
@@ -12,9 +39,14 @@ const NewPasswordPage = () => {
         <h3>Ingresa la nueva contraseña</h3>
       </div>
 
-      <form>
-        <InputPasswordForm placeholder="Contraseña" />
-        <InputPasswordForm placeholder="Repetir la contraseña" />
+      <form onSubmit={handleSubmit}>
+        <InputPasswordForm ref={passwordRef} placeholder="Contraseña" aria-label="NUeva contraseña" />
+        <InputPasswordForm
+          ref={passwordCheckRef}
+          placeholder="Repetir la contraseña" aria-label="Repetir nueva contraseña"
+        />
+
+        {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
 
         <Button variant="Primary">Establecer contraseña</Button>
       </form>
