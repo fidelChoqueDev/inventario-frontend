269f482159ff5efd5aa26c365f8fea6fd8553047
migueldecode <migueldecode@gmail.com>
2024-08-16 14:07:54 +0200

    feat: create a custom hook for fetch data

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+interface UseFetchOptions {
+  method?: string;
+  headers?: Record<string, string>;
+  body?: object | null;
+}
+
+export const useFetch = (url: string, options?: UseFetchOptions) => {
+  const [error, setError] = useState<Error | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [data, setData] = useState<object | null>(null);
+
+  const submit = async (customOptions?: UseFetchOptions) => {
+    setIsLoading(true);
+    setError(null);
+
+    const {
+      method = "POST",
+      headers = { "Content-Type": "application/json" },
+      body = null,
+    } = {
+      ...options,
+      ...customOptions,
+    };
+
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    try {
+      const response = await fetch(url, {
+        method,
+        headers,
+        body: body ? JSON.stringify(body) : null,
+        signal,
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      setData(result);
+    } catch (err) {
+      if (err.name !== "AbortError") {
+        setError(err);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+
+    return { data, error };
+  };
+
+  return { submit, error, isLoading, data };
+};
