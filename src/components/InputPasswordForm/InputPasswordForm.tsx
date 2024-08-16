b3221a87fcf416c9958cc3e0d9704ba81c672c19
migueldecode <migueldecode@gmail.com>
2024-08-16 19:36:00 +0200

    Revert "fix: remove the useRef and let the componet as original"
    This reverts commit 72752e5f8cdd7ae6c7c007af440d4b5c7f167353.

--- src/components/InputPasswordForm/InputPasswordForm.tsx
+++ src/components/InputPasswordForm/InputPasswordForm.tsx
@@ -1,4 +1,4 @@
-import { ForwardedRef, forwardRef, useState } from "react";
+import { useState } from "react";
 import "./InputPasswordForm.css";
 import Icon from "../Icon/Icon";
 
@@ -6,10 +6,7 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   error?: string;
 }
 
-const InputPasswordForm = forwardRef(function InputPasswordForm(
-  props: Props,
-  ref: ForwardedRef<HTMLInputElement>
-) {
+export default function InputPasswordForm(props: Props) {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
@@ -17,7 +14,6 @@ const InputPasswordForm = forwardRef(function InputPasswordForm(
       <input
         className="c-input-password"
         type={showPassword ? "text" : "password"}
-        ref={ref}
         {...props}
       />
       <div className="c-checkbox-show-password">
@@ -32,6 +28,4 @@ const InputPasswordForm = forwardRef(function InputPasswordForm(
       </div>
     </div>
   );
-});
-
-export default InputPasswordForm;
+}
