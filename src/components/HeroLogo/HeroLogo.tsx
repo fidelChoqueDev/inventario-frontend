0cc8df54380666b3216465224446ae591c1d5c4f
migueldecode <migueldecode@gmail.com>
2024-08-21 13:11:03 +0200

    feat: create a heroLogo component

---
+++ src/components/HeroLogo/HeroLogo.tsx
@@ -0,0 +1,11 @@
+import "./HeroLogo.css";
+
+const HeroLogo = () => {
+  return (
+    <div className="logo-container">
+      <img className="logo-icon" src="/logo.svg" alt="Box" />
+    </div>
+  );
+};
+
+export default HeroLogo;
