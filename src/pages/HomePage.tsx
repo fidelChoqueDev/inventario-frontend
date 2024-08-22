0cd81a2c5cdcd28b42c155f2fd423619cc27df82
migueldecode <migueldecode@gmail.com>
2024-08-22 10:56:00 +0200

    feat: refactor homepage with brand styles

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,29 @@
-import { Link } from 'react-router-dom'
-import './HomePage.css'
+import { Link } from "react-router-dom";
+import "./HomePage.css";
+import HeroLogo from "../components/HeroLogo/HeroLogo";
 
 const HomePage = () => {
   return (
-    <main>
-      <img src="/clean-box.png" alt="Box" />
-      <h1>Bienvenido a Zaiko</h1>
-      <h2>Gestiona tu inventario de manera precisa y eficiente</h2>
+    <main className="home__container">
+      <HeroLogo />
 
-<nav>
-      <Link className='btn btn-light' to='/register'>Registrarse</Link>
-      <Link className='btn btn-dark' to='/login'>Iniciar sesión</Link>
-</nav>
+      <header className="home__header">
+        <h1 className="home__title">Bienvenido</h1>
+        <h3 className="home__subtitle">
+          Gestiona tu inventario de manera precisa y eficiente
+        </h3>
+      </header>
+
+      <nav className="home__nav">
+        <Link className="link link--light" to="/register">
+          Registrarse
+        </Link>
+        <Link className="link link--dark" to="/login">
+          Iniciar sesión
+        </Link>
+      </nav>
     </main>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
