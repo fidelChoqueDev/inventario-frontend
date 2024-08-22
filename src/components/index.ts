5cd8968f9d2ca9e47900792ff064cd3e12fd0711
Alejandro25RA <alejito25raiders@gmail.com>
2024-08-22 11:03:30 -0500

    feat: create barrel in folder components

---
+++ src/components/index.ts
@@ -0,0 +1,33 @@
+import { default as Button } from "./Button/Button";
+import { default as ButtonBackIcon } from "./ButtonBackIcon/ButtonBackIcon";
+import { default as Checkbox } from "./Checkbox/Checkbox";
+import { default as CustomLink } from "./CustomLink/CustomLink";
+import { default as ErrorMessage } from "./ErrorMessage/ErrorMessage";
+import { default as HeroLogo } from "./HeroLogo/HeroLogo";
+import { default as Icon } from "./Icon/Icon";
+import { default as InputForm } from "./InputForm/InputForm";
+import { default as InputPasswordForm } from "./InputPasswordForm/InputPasswordForm";
+import { default as InputPhoneForm } from "./InputPhoneForm/InputPhoneForm";
+import { default as PasswordInputWithRequirements } from "./PasswordInputWithRequirements/PasswordInputWithRequirements";
+import { default as Popup } from "./Popup/Popup";
+import { default as ProtectedRoute } from "./ProtectedRoute/ProtectedRoute";
+import { default as Requirements } from "./Requirements/Requirements";
+import { default as SecurityQuestion } from "./SecurityQuestion/SecurityQuestion";
+
+export {
+  Icon,
+  Button,
+  ButtonBackIcon,
+  Checkbox,
+  CustomLink,
+  ErrorMessage,
+  HeroLogo,
+  InputForm,
+  InputPasswordForm,
+  InputPhoneForm,
+  PasswordInputWithRequirements,
+  Popup,
+  ProtectedRoute,
+  Requirements,
+  SecurityQuestion,
+};
