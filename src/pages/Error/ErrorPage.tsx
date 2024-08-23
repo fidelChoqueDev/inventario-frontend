f8ce10ad1ec5d8eaf1f68cbc1010a97b34ef4571
Miguel Decode <97762748+MiguelDecode@users.noreply.github.com>
2024-08-23 15:38:12 +0200

    Update ErrorPage.tsx
    Change image of background.

--- src/pages/Error/ErrorPage.tsx
+++ src/pages/Error/ErrorPage.tsx
@@ -5,8 +5,8 @@ const ErrorPage = () => {
   return (
     <main>
       <ButtonBackIcon goTo="/" />
-      <HeroLogo />
-      <h1>Lo sentimos, hemos perdido esa página.</h1>
+      <img src="/public/clean-box.png" alt="Caja vacia" />
+      <h1>Lo sentimos, hemos perdido esta página.</h1>
     </main>
   );
 };
